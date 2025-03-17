531e8615c2bbca181fd4168eedbdedee491d348f
Uren401 <aaron.wu.401@gmail.com>
2025-03-16 20:04:34 -0500

    Create StartPanel.java

---
+++ StartPanel.java
@@ -0,0 +1,44 @@
+
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.image.*;
+import javax.imageio.ImageIO;
+
+public class StartPanel extends Panel implements MouseListener {
+	private BufferedImage startScreen, gameButton, creditsButton;
+	
+	public StartPanel(Frame f) {
+		super(f);
+		addMouseListener(this);
+	}
+	
+	public void paint(Graphics g) {
+		g.drawImage(startScreen, 0, 0, 1640, 1530, null);
+
+		g.drawImage(Helper.applyOpacity(gameButton, 0.9f), 570,460, 500, 200, null);
+		g.drawImage(Helper.applyOpacity(creditsButton, 0.9f), 570, 760, 500, 200, null);
+	}
+	
+	public void mousePressed(MouseEvent e) {
+		int x = e.getX();
+		int y = e.getY();
+		
+		if (x >= 570 && x < 1070 && y >= 460 && y < 660) {
+			getFrame().toGame();
+		}
+		if (x >= 570 && x <= 1070 && y >= 760 && y < 960) {
+		}
+	}
+	
+	public void loadImages() {
+		try {
+			startScreen = ImageIO.read(Panel.class.getResource("/Images/startScreen.png"));
+			gameButton = ImageIO.read(Panel.class.getResource("/Images/gameButton.png"));
+			creditsButton = ImageIO.read(Panel.class.getResource("/Images/creditsButton.png"));
+		} catch (Exception E) {
+			System.out.println("FIRE IN THE Start!");
+			return;
+		}
+	}
+}
