5972d80cf6f681107d1c04e6b3084cf0be463bd8
Karen Wang <karenwangusa@outlook.com>
2025-04-02 13:57:47 +0000

    change
    Co-authored-by: sarahGordon2 <sarahGordon2@users.noreply.github.com>

--- GameState.java
+++ GameState.java
@@ -1,6 +1,6 @@
 
 import java.util.*;
-
+import java.io.*;
 public class GameState{
 
     //private Player[] playerArr;
@@ -15,21 +15,30 @@ public class GameState{
        // ArrayList<City>
         //createMap();
         
-        ArrayList<City> cities = readCities();
+        //ArrayList<City> cities = readCities();
+        try{
+            createMap(readCities());
+            System.out.println("success");
+        }
+        catch(Exception e){
+        System.out.println("error");
+        }
     }
 
     public static void main(String[] args) {
         System.out.println("hello");
+        GameState g = new GameState();
         
     }
 
-    public void createMap(ArrayList<City> cities){
+    public void createMap(ArrayList<City> cities) throws IOException{
         board = new HashMap<>();
-        Scanner sc = new Scanner("routes.txt");
+        Scanner sc = new Scanner(new File("routes.txt"));
         ArrayList<Route> routes = new ArrayList<Route>();
         City city1, city2;
         while(sc.hasNextLine()){
           String[] info = sc.nextLine().split(" ");
+          System.out.println(Arrays.toString(info));
           city1 = searchCity(info[0], cities);
           city2 = searchCity(info[1], cities);
           routes.add(new Route(city1, city2, Integer.parseInt(info[2]), info[3], Boolean.valueOf(info[4]), Integer.parseInt(info[5])));
@@ -55,8 +64,9 @@ public class GameState{
         return null;
     }
 
-    public ArrayList<City> readCities(){
-        Scanner sc = new Scanner("cities.txt");
+    public ArrayList<City> readCities() throws IOException{
+        System.out.println("read cities");
+        Scanner sc = new Scanner(new File("cities.txt"));
         ArrayList<City> cities = new ArrayList<>();
         while(sc.hasNextLine()){
             cities.add(new City(sc.nextLine()));
--- Ticket.java
+++ Ticket.java
@@ -1,1 +1,12 @@
-public 
+public class Ticket{
+
+    private City city1, city2;
+    private int points;
+    private boolean completed;
+
+    public ticket(City city1, City city2, int p){
+
+        this.city1 = city1;
+        this.city2 = city2;
+    }
+}
