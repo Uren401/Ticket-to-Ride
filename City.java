7820b19ecb817ae67f7a94f8667d51c7e7eeb070
Karen Wang <karenwangusa@outlook.com>
2025-04-03 14:33:10 +0000

    change

--- City.java
+++ City.java
@@ -1,15 +1,22 @@
 
-public class City{
+public class City {
     private String name;
-
+    private Player station;
     public City(String n){
         name = n;
+        station = null;
     }
 
     public String name(){
         return name;
     }
+    
     public String toString(){
         return name;
+    }
+    public 
+ dio
+    public String getr(){
+        return station;
     }
 }
