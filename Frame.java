fd368070b8f047e58bc0358cc52af5236799e120
Uren401 <aaron.wu.401@gmail.com>
2025-04-21 11:34:42 -0500

    Update Frame.java

--- Frame.java
+++ Frame.java
@@ -1,10 +1,8 @@
 
-import java.io.*;
-import java.util.*;
 import javax.swing.*;
 
 public class Frame extends JFrame {
-	private static final int WIDTH = 1550, HEIGHT = 1140;
+	private static final int WIDTH = 1550, HEIGHT = 1100;
 	private Panel[] panels = new Panel[3];
 	
 	public Frame(String framename) {
@@ -23,11 +21,6 @@ public class Frame extends JFrame {
 	public void toGame() {
 		remove(panels[0]);
 		add(panels[1]);
-		try {
-			paintButtons();
-		} catch (Exception e) {
-			System.out.println("FIRE IN THE Frame!" + e.getMessage());
-		}
 		revalidate();
 		repaint();
 	}
@@ -37,22 +30,5 @@ public class Frame extends JFrame {
 		add(panels[2]);
 		revalidate();
 		repaint();
-	}
-
-	public void paintButtons() throws IOException {
-		Scanner sc;
-		try {
-			sc = new Scanner(new File("cities.txt"));
-		} catch (Exception e) {
-			System.out.println("FIRE IN THE Frame!");
-			return;
-		}
-		
-		while (sc.hasNext()) {
-			String[] split = sc.nextLine().split(" ");
-			JButton city = new JButton();
-			city.setBounds(Integer.parseInt(split[1]) - 10, Integer.parseInt(split[2]) - 10, 20, 20);
-			panels[1].add(city);
-		}
 	}
 }
